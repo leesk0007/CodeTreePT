ce73353b581ee1e17308214e447d24b6ad35bd5c
leesk0007 <leesk000107@naver.com>
2024-12-31 00:52:33 +0900

    Title: 자동차 단일 거래 이익 최대화하기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 241231/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
+++ 241231/자동차 단일 거래 이익 최대화하기/max-profit-of-single-car.c
@@ -1,35 +1,30 @@
 #include <stdio.h>
 
-int main() {
-    // 여기에 코드를 작성해주세요.
-    int i, index, n, max=0, min=2147483647, arr[1000], result=0;
-    // n 입력받고 
-    scanf("%d",&n);    
-    
-    // 배열에 입력받고
-    for(i=0; i<n; i++)
-        scanf("%d",&arr[i]);    
-    
-    // 가장 쌀 때 사는거야 그리고 그 인덱스 체크
-    for(i=0; i<n; i++){ 
-        if(min>arr[i]){
-            min = arr[i]; //샀음
-            index = i;
-        }
-    }
-    //팔아야지
-    for(i=index+1; i<n; i++){ 
-        if(max<arr[i]){
-            max = arr[i]; //샀음
-            
-        }
-    }
-    result = (max-min);
+#define MAX_NUM 1000
 
-    if(result <= 0)
-        printf("0");
-    else
-        printf("%d",result);
-    
+int main() {
+    // 변수 선언:
+    int n;
+    int price[MAX_NUM];
+
+    // 입력:
+    scanf("%d", &n);
+    for(int i = 0; i < n; i++)
+        scanf("%d", &price[i]);
+
+    // 배열을 앞에서부터 순회하며 사는 시점의 후보를 선택합니다
+    int max_profit = 0;
+    for(int i = 0; i < n; i++) {
+        // 사는 시점의 다음 해부터 순회하며 파는 시점의 후보를 선택합니다
+        for(int j = i + 1; j < n; j++) {
+			int profit = price[j] - price[i];
+            
+            if(profit > max_profit)
+                max_profit = profit;
+        }
+    }
+
+    printf("%d", max_profit);
+	
     return 0;
 }
