decea330261b14717debc6ff22910fa2be11b0e2
Seonggyu Lee <leesk000107@naver.com>
2026-02-08 18:54:09 +0900

    Title: 병합 정렬 구현, Time: 1211ms, Memory: 34MB, Status: Passed - Codetree

--- 260208/병합 정렬 구현/implement-merge-sort.java
+++ 260208/병합 정렬 구현/implement-merge-sort.java
@@ -1,71 +1,59 @@
 import java.util.Scanner;
 
 public class Main {
-
-    public static int[] re_arr;
+    public static int[] temp; 
 
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
         int[] arr = new int[n];
-        re_arr = new int[n];
+        
+        temp = new int[n]; 
+
         for (int i = 0; i < n; i++) {
             arr[i] = sc.nextInt();
         }
-        // Please write your code here.
 
-        merge(arr, 0, n-1);
+        mergeSort(arr, 0, n - 1);
 
         for (int i = 0; i < n; i++) {
             System.out.print(arr[i] + " ");
         }
     }
 
-    public static void merge(int arr[], int low, int high){
-        if(low < high){
+    public static void mergeSort(int[] arr, int low, int high) {
+        if (low < high) {
             int mid = (low + high) / 2;
-            merge(arr, low, mid);
-            merge(arr, mid + 1, high);
-            merge_sort(arr, low, mid, high);
+            mergeSort(arr, low, mid);
+            mergeSort(arr, mid + 1, high);
+            merge(arr, low, mid, high);
         }
     }
 
-
-    public static void merge_sort(int arr[], int low, int mid, int high){
+    public static void merge(int[] arr, int low, int mid, int high) {
         int i = low;
         int j = mid + 1;
-
         int k = low;
-        
 
-        // 두 배열이 모두 비어있지 않다면
-        while(i <= mid && j <= high){
-            if(arr[i] < arr[j]){
-                re_arr[k] = arr[i];
-                i++;
-                k++;
-            }
-            else {
-                re_arr[k] = arr[j];
-                j++;
-                k++;
+
+        while (i <= mid && j <= high) {
+            if (arr[i] < arr[j]) {
+                temp[k++] = arr[i++]; 
+            } else {
+                temp[k++] = arr[j++];
             }
         }
-        // 1번 배열이 비어있지않다면
-        if(i < mid + 1){
-            for(int f = i; f <= mid; f++)
-                re_arr[k++] = arr[i];
-                i++;
+
+        while (i <= mid) {
+            temp[k++] = arr[i++];
         }
-        // 2번 배열이 비어있지않다면
-        else if(j < high + 1){
-            for(int f = j; f <= high; f++)
-                re_arr[k++] = arr[j];
-                j++;
+
+        while (j <= high) {
+            temp[k++] = arr[j++];
         }
-        
-        for(int index = low; index <= high; index++) {
-            arr[index] = re_arr[index];
-        }        
+
+        for (int index = low; index <= high; index++) {
+            arr[index] = temp[index];
+        }
     }
 }
